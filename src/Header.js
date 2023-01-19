c2abf7d0539362e2aee501b1b190a0bc53ab6975
MaryamAl-M <Mariam.almashhadi14@gmail.com>
2023-01-19 16:07:41 +0100

    Creating the app

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import react,{Component} from 'react'
+
+class Header extends Component{
+    render()
+    {
+        return (
+            <h1>This is the Header</h1>
+        )
+    }
+}
+export default Header;
