d808e152fe8a664dccea9957a3464b863c4a38f3
MaryamAl-M <Mariam.almashhadi14@gmail.com>
2023-01-23 11:42:56 +0100

    feat: loginfunction

--- src/views/Startup.jsx
+++ src/views/Startup.jsx
@@ -1,6 +1,12 @@
+import LoginForm from '../components/Startup/LoginForm'
+
 const Startup = () => {
     return (
-        <h1>Startup</h1>
+        <>
+              <h1>Startup</h1>
+              <LoginForm />
+        </>
+  
     )
 }
 export default Startup;
