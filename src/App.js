f4873eeacad9e431ace5de18721b2a18c67d8e26
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-01-27 11:42:37 +0100

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import  Navbar  from './components/Navbar/Navbar';
-
-import {
-  BrowserRouter,
-  Routes, 
-  Route
-} from 'react-router-dom'
-import Startup from "./views/Startup";
-import Profile from "./views/Profile";
-import Translations from "./views/Translate";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Startup from "./views/startup/Startup";
+import Profile from "./views/profile/Profile";
+import Translate from "./views/translate/Translate";
+import { Provider } from "./context/userProvider";
+import { Header } from "./components/header/Header";
 
 function App() {
-  return(
-
-  <BrowserRouter>
-
-
-  <div className="App">
-  <Navbar />
-   <Routes>
-    <Route path="/" element={ <Startup /> } />
-    <Route path="/Translate" element={ <Translations /> } />
-    <Route path="/Profile" element={ <Profile /> } />
-   </Routes>
-  </div>
-
-
-
-  </BrowserRouter>
+  return (
+    <Provider>
+      <BrowserRouter>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Startup />} />
+            <Route path="/Translate" element={<Translate />} />
+            <Route path="/Profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
-
 }
 
 export default App;
