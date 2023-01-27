129101ab776deb02b5f5b119237b4def85a8fb1f
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-01-27 13:59:34 +0100

    Fix context in index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css"
-import AppContext from './context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppContext>
-       <App />
-    </AppContext>
+    <App />
   </React.StrictMode>
 );
 
